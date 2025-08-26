d5426209618c201e5fe1fbe8ce975e7aac0ce17e
amin-s81 <amin.sanei81@gmail.com>
2025-08-26 18:48:50 +0330

    migrate

--- hours/esfa_eyes/models.py
+++ hours/esfa_eyes/models.py
@@ -1,45 +1,50 @@
 from django.db import models
-from functools import partial
-from schemas.esfa_eyes_info import *
+from .schemas import esfa_eyes_info as schemas
+from .schemas.esfa_eyes_info import EsfaEyesInfo, EsfaEyesMonltyInfo, EsfaEyesProductInfo
 
-    
+def default_financial_info():
+    return {
+        'balance_rials_official': EsfaEyesInfo(0),
+        'balance_rials': EsfaEyesInfo(0),
+        'montly_checks_recieved': EsfaEyesMonltyInfo(),
+        'montly_installment': EsfaEyesMonltyInfo(),
+        'montly_total_sales': EsfaEyesMonltyInfo(),
+        'individual_sales': EsfaEyesProductInfo(),
+        'total_insured_staffs': EsfaEyesInfo(0),
+        'total_uninsured_staffs': EsfaEyesInfo(0),
+        'total_salary_paid': EsfaEyesInfo(0),
+        'total_insurance_paid': EsfaEyesInfo(0),
+    }
+
+
+def default_international_finance_info():
+    return {
+        "balance_dollars": {},
+        "china_production_orders": EsfaEyesProductInfo(),
+    }
+
+
+def default_international_sales_info():
+    return {
+        "montly_international_total_sales": EsfaEyesMonltyInfo(),
+        "international_individual_sales": EsfaEyesProductInfo(),
+        "china_production_orders": EsfaEyesProductInfo(),
+    }
+
+
+def default_products_info():
+    return {
+        "unproduced_workshop_inventory": EsfaEyesProductInfo(),
+        "ready_products": EsfaEyesProductInfo(),
+    }
+
+
 class EsfaEyes(models.Model):
-    year = models.CharField('year', max_length=4, default='1404')
-    financial_info = models.JSONField(default=partial(dict, 
-        {
-            'balance_rials_official': EsfaEyesInfo(0),
-            'balance_rials': EsfaEyesInfo(0),
-            'montly_checks_recieved': EsfaEyesMonltyInfo(),
-            'montly_checks_issued': EsfaEyesMonltyInfo(),
-            'montly_installment ': EsfaEyesMonltyInfo(),
-            'montly_total_sales': EsfaEyesMonltyInfo(),
-            'individual_sales': EsfaEyesProductInfo(),
-            'total_insured_staffs': EsfaEyesInfo(0),
-            'total_uninsured_staffs': EsfaEyesInfo(0),
-            'total_salary_paid': EsfaEyesInfo(0),
-            'total_insurance_paid': EsfaEyesInfo(0),
-        }
-    ))
-    international_finance_info = models.JSONField(default=partial(dict, 
-        {
-            'balance_dollars': EsfaEyesInfo({}),
-            'china_production_orders': EsfaEyesProductInfo(),
-        }
-    ))
-    international_sales_info = models.JSONField(default=partial(dict, 
-        {
-            'montly_international_total_sales': EsfaEyesMonltyInfo(),
-            'international_individual_sales': EsfaEyesProductInfo(),
-            'china_production_orders': EsfaEyesProductInfo(),
-        }
-    ))
-    products_info = models.JSONField(default=partial(dict, 
-        {
-            'unproduced_workshop_inventory': EsfaEyesProductInfo(),
-            'ready_products': EsfaEyesProductInfo(),
-        }
-    ))
-
+    year = models.CharField("year", max_length=4, default="1404")
+    financial_info = models.JSONField(default=default_financial_info)
+    international_finance_info = models.JSONField(default=default_international_finance_info)
+    international_sales_info = models.JSONField(default=default_international_sales_info)
+    products_info = models.JSONField(default=default_products_info)
 
     def get(self, info=None):
         """Return a DateInfo instance.
@@ -49,7 +54,7 @@ class EsfaEyes(models.Model):
         """
         if info is None:
             info = {
-                'financial_info': self.financial_info,
-                'international_finance_info': self.international_finance_info,
+                "financial_info": self.financial_info,
+                "international_finance_info": self.international_finance_info,
             }
         return self.DateInfo(info)
