303d71aba3c5b5030c0b91eca33728a1820f41a0
amin-s81 <amin.sanei81@gmail.com>
2025-09-08 18:30:08 +0330

    make tables fix in super user mode

--- hours/esfa_eyes/static/esfa_eyes/js/templatesScripts/esfa_eyes_dashboard.js
+++ hours/esfa_eyes/static/esfa_eyes/js/templatesScripts/esfa_eyes_dashboard.js
@@ -1,5 +1,5 @@
 let originalApiData = {};
-let Debug = true;
+let Debug = false;
 
 const titleMapping = {
 	balance_rials_official: 'موجودی حساب‌های رسمی',
@@ -132,7 +132,7 @@ function createNumericTable(data, title, itemKeys, editable = false) {
 		const [datePart, timePart] = item.last_modify_time.split(' ');
 		const jdate = new JDate(datePart.split('-').map(Number));
 		const formattedDate = jdate.format('YYYY-MM-DD') + ' ' + timePart.substring(0, 5);
-		tableBody += `<tr class="${bgColor}"><td>${titleMapping[key] || key}</td><td contenteditable="true" data-key="${key}">${item._info.toLocaleString()}</td><td>${formattedDate}</td></tr>`;
+		tableBody += `<tr class="${bgColor}"><td>${titleMapping[key] || key}</td><td contenteditable="${editable}" data-key="${key}">${item._info.toLocaleString()}</td><td>${formattedDate}</td></tr>`;
 	});
 	const cardBorderColor = cardBgColor.replace('bg-', 'border-').replace('-subtle', '');
 	const cardFooter = editable ? `
@@ -188,7 +188,7 @@ function createObjectTable(data, title, itemKeys, editable=false) {
 		const formattedDate = jdate.format('YYYY-MM-DD') + ' ' + timePart.substring(0, 5);
 		let row = `<tr class="${bgColor}"><td>${titleMapping[key] || key}</td>`;
 		subItemHeaders.forEach(header => {
-			row += `<td contenteditable="true" data-key="${key}" data-subkey="${header}">${(item._info[header] || 0).toLocaleString()}</td>`;
+			row += `<td contenteditable="${editable}" data-key="${key}" data-subkey="${header}">${(item._info[header] || 0).toLocaleString()}</td>`;
 		});
 		row += `<td>${formattedDate}</td></tr>`;
 		tableBody += row;
