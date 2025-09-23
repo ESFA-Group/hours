f6eeb7e6219ba5c1ce1eaae759901822baf03822
amin-s81 <amin.sanei81@gmail.com>
2025-09-23 18:28:00 +0330

    init services test

--- Hiddify/vpn.sh
+++ Hiddify/vpn.sh
@@ -1,1 +1,17 @@
-HiddifyCli run -c https://de11pooya.1qwertyuiopasdfghjklzxcvbnmmnbvcxzlkjhgfdsapoiuytrewq1234567890.com:2096/1qwertyuiopasdfghjklzxcvbnmmnbvcxzlkjhgfdsapoiuytrewq1234567890/ESFAGroup_2
+cleanup() {
+if kill -0 $VPN_PID 2>/dev/null; then
+kill $VPN_PID
+fi
+}
+
+trap cleanup EXIT
+
+HiddifyCli run -c https://de11pooya.1qwertyuiopasdfghjklzxcvbnmmnbvcxzlkjhgfdsapoiuytrewq1234567890.com:2096/1qwertyuiopasdfghjklzxcvbnmmnbvcxzlkjhgfdsapoiuytrewq1234567890/ESFAGroup_2 &
+VPN_PID=$!
+
+sleep 5
+
+cd /home/mrn/hours/
+/home/mrn/hours/venv/bin/python manage.py check_submissions --dry-run
+
+exit 0
